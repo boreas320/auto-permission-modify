033a1bc3ae30d7ce2d5f87d17d6c141742a5db2d
相帅 <xiangshuai@meituan.com>
2016-01-11 02:31:29 +0800

    Main logic done

---
+++ src/AutoModifyPermissions.java
@@ -0,0 +1,43 @@
+//package org.boreas320.auto.permissiom.modify;
+
+import com.sun.nio.file.SensitivityWatchEventModifier;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.List;
+
+import static java.nio.file.StandardWatchEventKinds.*;
+
+public class AutoModifyPermissions {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        WatchService watchService = FileSystems.getDefault().newWatchService();
+        Path path = Paths.get(args[0]);
+        path.register(watchService, new WatchEvent.Kind[]{ENTRY_CREATE}, SensitivityWatchEventModifier.HIGH);
+        System.out.println("ready to watch!");
+        while (true) {
+
+            WatchKey key = watchService.take();
+            List<WatchEvent<?>> watchEvents = key.pollEvents();
+
+
+            watchEvents.forEach(watchEvent -> {
+                WatchEvent.Kind<?> kind = watchEvent.kind();
+                if (kind == OVERFLOW) {
+                    return;
+                }
+                Path context = (Path) watchEvent.context();
+                Path resolve = path.resolve(context);
+                File created = resolve.toFile();
+                if (!created.canExecute()) {
+                    boolean b = created.setExecutable(true, false);
+                    System.out.printf("set %s exists %b executable %b \n", created, created.exists(), b);
+                }
+            });
+            key.reset();
+
+        }
+
+    }
+}
--- src/org/boreas320/auto/permissiom/modify/Main.java
+++
@@ -1,8 +0,0 @@
-package org.boreas320.auto.permissiom.modify;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
